5beea8ecf076da9a4667691af028695962463246
Nacho <skippnacho@gmail.com>
2026-01-24 20:09:19 -0500

    Agregar Dockerfile y manifests de Kubernetes

---
+++ docker/app/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Hola desde version 2 🚀");
+
+});
+
+/*
+❌ ERROR INTENCIONAL
+Solo escucha en localhost
+*/
+server.listen(3000, () => {
+  console.log("App escuchando en localhost:3000");
+});
+
